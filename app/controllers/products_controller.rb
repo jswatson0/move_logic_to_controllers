3f88ea0b60a098c02dbc4500fcc62bb722e4eac0
Jason Watson <jswatson0@gmail.com>
2013-07-04 15:46:24 -0500

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,4 +25,27 @@ class ProductsController < ApplicationController
 	def new
     @product = Product.new
   end
+
+  def show
+  	@product = Product.find(params[:id])
+  end
+
+  def edit
+  	@product = Product.find(params[:id])
+  end
+
+  def update
+  @product = Product.where(:id => params[:id]).first
+
+	  respond_to do |format|
+	    if @product.update_attributes(params[:product])
+	      format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+	      format.json { head :no_content }
+	    else
+	      format.html { render action: "edit" }
+	      format.json { render json: @product.errors, status: :unprocessable_entity }
+	    end
+	  end
+	end	
 end
+
